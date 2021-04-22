aedb49bfe0f0fbb367782bcfa1c3e28d7128b24a
Heather Wilson <hdaniwilson@gmail.com>
2021-04-22 22:02:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,11 +2,11 @@ def starts_with_a_vowel?(word)
   if word.match(/\A+[aeiouAEIOU]/)
     true
   else false
-  end 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/i)
 end
 
 def words_five_letters_long(text)
